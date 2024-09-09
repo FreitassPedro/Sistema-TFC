152a53ab2d8bc790ab93e0054064b788ffaa03ab
FreitasPedro <pedrospf2018@gmail.com>
2024-09-09 20:32:41 -0300

    :sparkles: feat: Registrar Venda Form
    - Adicao apenas visual

--- frontend/src/components/VendaForm/index.tsx
+++ frontend/src/components/VendaForm/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import "./styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const VendaForm: React.FC = () => {
   // Crie um formulário para adicionar uma nova venda
 
@@ -11,74 +15,82 @@ const VendaForm: React.FC = () => {
       setNomes(novosNomes);
     };
 
-    const adicionarNome = () => {
-        setNomes([...nomes, ""]);
-    }
-    const removerNome = (index: number) => {
-        const novosNomes = [...nomes];
-        novosNomes.splice(index, 1);
-        setNomes(novosNomes);
-    }
-    
+  const adicionarNome = () => {
+    setNomes([...nomes, ""]);
+  };
+  const removerNome = (index: number) => {
+    const novosNomes = [...nomes];
+    novosNomes.splice(index, 1);
+    setNomes(novosNomes);
+  };
+
   return (
-    <div className="form-container">
-      <form action="#">
-        <div className="title">Inserir Venda</div>
-        <div className="login-button">
-          <button type="submit">Inserir</button>
-        </div>
-
-        <div className="input-group">
-          <div className="input-box">
-            <label htmlFor="instagram">Insta @</label>
-            <input
-              id="instagram"
-              name="instagram"
-              type="text"
-              placeholder="@"
-              required
-            />
-          </div>
-        </div>
-
-        <div className="input-group">
-          <div className="input-box">
-            <label htmlFor="valorIngresso">Preço</label>
-            <input
-              id="preco"
-              name=""
-              type="text"
-              placeholder="Valor do ingresso"
-              required
-            />
-          </div>
-        </div>
-
-        <div className="input-group">
-          {nomes.map((nome, index) => (
-            <div className="input-box" key={index}>
-              <label htmlFor={`nome-{$index}`}>Nome {index + 1}</label>
-              <input
-                id={`nome-${index}`}
-                type="text"
-                value={nome}
-                placeholder="Insira o nome"
-                onChange={(e) => handleNomeChange(index)(e)}
-                required
-              />
-              {index > 0 && (
-                <button type="button" onClick={() => removerNome(index)}>
-                  Remover
-                </button>
-              )}
+    <div className="base-card home">
+      <div className="form-container">
+        <div className="form base-card">
+          <form action="#">
+            <div className="form-header ">
+              <div className="form-title">
+                <h1>Registrar Venda</h1>
+              </div>
+              <div className="send-button">
+                <button type="submit">Inserir</button>
+              </div>
             </div>
-          ))}
+            <div className="input-group">
+              <div className="input-box">
+                <label htmlFor="instagram">Comprovante</label>
+                <input
+                  id="instagram"
+                  name="instagram"
+                  type="text"
+                  placeholder="Digite @... do comprovante"
+                  required
+                />
+              </div>
+            </div>
 
-          <button type="button" onClick={adicionarNome}>
-            Adicionar nome
-          </button>
+            <div className="input-group">
+              <div className="input-box">
+                <label htmlFor="valorIngresso">Preço</label>
+                <input
+                  id="preco"
+                  name=""
+                  type="text"
+                  placeholder="Digite valor do ingresso"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="input-group">
+              {nomes.map((nome, index) => (
+                <div className="input-box" key={index}>
+                  <label htmlFor={`nome-{$index}`}>Nome {index + 1}</label>
+                  <input
+                    id={`nome-${index}`}
+                    type="text"
+                    value={nome}
+                    placeholder="Insira o nome"
+                    onChange={(e) => handleNomeChange(index)(e)}
+                    required
+                  />
+                  {index > 0 && (
+                    
+                      <button type="button" onClick={() => removerNome(index)}>
+                        <FontAwesomeIcon icon={faTrash} />
+                      </button>
+                  )}
+                </div>
+              ))}
+
+              <button type="button" onClick={adicionarNome} className="button-add-name">
+                Adicionar nome
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
