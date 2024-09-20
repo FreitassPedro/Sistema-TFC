d4b0ae69f06fce57e402dfb45003c8f3572dd932
FreitasPedro <pedrospf2018@gmail.com>
2024-09-20 20:51:21 -0300

    :lipstick: feat: Estilização Ingresso Impresso

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -8,10 +8,10 @@ INSERT INTO tb_transacao(id, data_transacao, valor, quantia_ingressos, instagram
 INSERT INTO tb_transacao(id, data_transacao, valor, quantia_ingressos, instagram_comprovante) VALUES (8, '2024-10-20 05:00:00', 50.00, 1, '@teste');
 INSERT INTO tb_transacao(id, data_transacao, valor, quantia_ingressos, instagram_comprovante) VALUES (9, '2024-10-20 05:00:00', 50.00, 1, '@teste');
 
-INSERT INTO tb_cliente(id, nome) VALUES (1, 'Maria');
-INSERT INTO tb_cliente(id, nome) VALUES (2, 'João');
-INSERT INTO tb_cliente(id, nome) VALUES (3, 'Pedro');
-INSERT INTO tb_cliente(id, nome) VALUES (4, 'Ana');
+INSERT INTO tb_cliente(id, nome) VALUES (1, 'Maria Clara de Souza');
+INSERT INTO tb_cliente(id, nome) VALUES (2, 'João da Silva Pereira');
+INSERT INTO tb_cliente(id, nome) VALUES (3, 'Pedro Henrique de Oliveira');
+INSERT INTO tb_cliente(id, nome) VALUES (4, 'Ana Carolina da Silva');
 
 INSERT INTO tb_ingresso(id, transacao_id, cliente_id, codigo_consumivel, valor, copos_disponiveis) VALUES (1, 1, 1, '123456', 50.00, 2);
 INSERT INTO tb_ingresso(id, transacao_id, cliente_id, codigo_consumivel, valor, copos_disponiveis) VALUES (2, 1, 2, '321DMA', 50.00, 3);
--- frontend/src/pages/IngressoImpresso/index.tsx
+++ frontend/src/pages/IngressoImpresso/index.tsx
@@ -9,7 +9,7 @@ import { Ingresso } from "types/Ingresso";
 const IngressoImpresso = () => {
   const { codigoConsumivel } = useParams();
   const [ingresso, setIngresso] = useState<Ingresso | null>(null);
-  
+
   useEffect(() => {
     axios
       .get(`${BASE_URL}/api/ingresso/code/${codigoConsumivel}`)
@@ -18,14 +18,45 @@ const IngressoImpresso = () => {
         console.log(response.data);
       })
       .catch((error) => {
-        console.error('Erro ao buscar dados do ingresso:', error);
+        if (error.response.status === 404) {
+          alert("Ingresso não encontrado");
+        }
+        console.error("Erro ao buscar dados do ingresso:", error);
       });
   }, []);
+
+  const diasRestantes = () => {
+    const dataEvento = new Date("2024-10-25T23:00:00.000Z");
+
+    // Pegue a data atual ao acessar
+    const dataAtual = new Date();
+
+    // Calcule a diferença entre as datas
+    const diferencaEmMilisegundos = dataEvento.getTime() - dataAtual.getTime();
+
+    // Converte a diferença em dias
+    const diferencaEmDias = Math.floor(
+      diferencaEmMilisegundos / (1000 * 60 * 60 * 24)
+    );
+
+    if (diferencaEmDias < 1) {
+      const diferencaEmHoras = Math.floor(
+        diferencaEmMilisegundos / (1000 * 60 * 60)
+      );
+      return `${diferencaEmHoras} horas`;
+    }
+    return `${diferencaEmDias} dias`;
+  };
 
   return (
     <>
       <div className="ingresso-impresso">
-        <div className="nft">
+        
+        <div className="bg-text">
+          <h1>TFC</h1>
+        </div>
+
+        <div className="card-main">
           <div className="main">
             <img
               className="box-image"
@@ -33,18 +64,25 @@ const IngressoImpresso = () => {
               alt="QR Code"
             />
             <div className="code-text">
-              <h2>{ingresso ? ingresso.codigoConsumivel : 'Código Inválido'}</h2>
+              <h2>
+                {ingresso
+                  ? `ID: ${ingresso.codigoConsumivel}`
+                  : "Código Inválido"}
+              </h2>
             </div>
             <hr />
-            <h2 className="card-nome">{ingresso ? ingresso.nome : 'Carregando...'}</h2>
+            <h2 className="card-nome">
+              {ingresso ? ingresso.nome : "Carregando..."}
+            </h2>
 
             <div className="card-info">
               <div className="date">
                 <span>◷</span>
-                <p>XX dias restantes</p>
+                <p>{diasRestantes()} restantes</p>
               </div>
             </div>
             <hr />
+            <span>Aproveite a festa!</span>
           </div>
         </div>
       </div>
